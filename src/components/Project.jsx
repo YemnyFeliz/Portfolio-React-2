b58192dda946b4ee643c49573f872fd3b0a22e3f
Yemny <y.feliz1296@gmail.com>
2023-12-03 15:17:02 -0500

    created cards with projects and added some style

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,29 @@
 import '../styles/Project.css';
+import { Card, Button } from 'react-bootstrap';
+
+const Project = ({ title, description, image, link, repo }) => {
+    return (
+      <Card>
+        <Card.Img src={image} alt={`${title} project`} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+          <div className="button-container">
+            <Button className="button" href={link} target="_blank" rel="noopener noreferrer">
+              {/* <CgWebsite className="icon m-1"/> */}
+              web
+            </Button>
+            <Button className="button" href={repo} target="_blank" rel="noopener noreferrer">
+              {/* <FaGithub className="icon"/> Github */}
+              GitHub
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+    );
+  };
+
+  export default Project;
 
 
 
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import Project from '../components/Project';
+import projects from '../utils/projects';
+
+import { Container, Row, Col } from 'react-bootstrap';
+
+
 function Porfolio() {
-return <div>THIS IS THE Porfolio PAGE</div>;
+return (
+    <Container className="portfolio-section" id="portfolio">
+    <h2>Projects</h2>
+    <Row>
+      {projects.map((project, index) => (
+        <Col key={index} sm={6} md={4} lg={4}>
+          <Project
+            title={project.title}
+            description={project.description}
+            image={project.image}
+            link={project.link}
+            repo={project.repo}
+          />
+        </Col>
+      ))}
+    </Row>
+  </Container>
+)
 }
 export default Porfolio;
